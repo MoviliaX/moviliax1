b693222c1b5d0a6fe4a06b4a17e43a66e60f9cfe
MoviliaX <hmanuelinnovaciondigital@gmail.com>
2025-10-14 00:03:53 -0600

    Create index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,26 @@
+// src/lib/index.ts
+// Exportaciones centralizadas de utilidades MoviliaX
+
+// Utilidades de formato
+export * from './format';
+
+// Utilidades de validación
+export * from './validation';
+
+// Utilidades de fecha
+export * from './date';
+
+// Utilidades de SEO
+export * from './seo';
+
+// Utilidades de MDX
+export * from './mdx';
+
+// Constantes del proyecto
+export * from './constants';
+
+// Tipos compartidos
+export * from './types';
+
+// Utilidades de API
+export * from './api';
