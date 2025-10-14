ab60aeee6b799f667060276a83118e47dc0047d9
MoviliaX <hmanuelinnovaciondigital@gmail.com>
2025-10-13 23:37:18 -0600

    Create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,25 @@
+// src/components/index.ts
+// Exportaciones centralizadas de componentes de MoviliaX
+
+// Layout Components
+export { Header } from './layout/Header';
+export { Footer } from './layout/Footer';
+export { Navbar } from './layout/Navbar';
+export { Sidebar } from './layout/Sidebar';
+
+// Common Components
+export { Button } from './common/Button';
+export { Card } from './common/Card';
+export { Logo } from './common/Logo';
+export { SearchBar } from './common/SearchBar';
+
+// Feature Components
+export { ArticleCard } from './features/ArticleCard';
+export { AuthorCard } from './features/AuthorCard';
+export { CategoryFilter } from './features/CategoryFilter';
+export { Newsletter } from './features/Newsletter';
+
+// UI Components
+export { Input } from './ui/Input';
+export { Badge } from './ui/Badge';
+export { Container } from './ui/Container';
